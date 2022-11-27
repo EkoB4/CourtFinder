c9b88ccc1846d26dc5ee321f363481fa3280d827
EkoB4 <ekinbacik@gmail.com>
2022-11-28 02:03:30 +0300

    new code addd

--- courtLocation/View/FindLocation.swift
+++ courtLocation/View/FindLocation.swift
@@ -21,29 +21,40 @@ struct FindLocation<CourtView : View, GenericViewModal : View>: View {
                         (annonitions) -> MapAnnotation<PinView> in MapAnnotation(coordinate:annonitions.coordinates) {
                             PinView(title: annonitions.name)
                         }
-                    }.frame(width: UIScreen.main.bounds.width*1.2, height: 600).ignoresSafeArea()
-                    Spacer(minLength: 20)
+                    }.frame(width: UIScreen.main.bounds.width*1.2, height: 650).ignoresSafeArea()
+                    Spacer(minLength: 10)
                 }
-                HStack{
-                        NavigationLink(destination: {
-                            FirstView
-                        }, label: {
-                            RoundedRectangle(cornerRadius: 20).frame(height: 180)
-                                .overlay(Text("Sa").foregroundColor(.black).background(RoundedRectangle(cornerRadius: 10).fill(Color.red).frame(width:200,height: 120)))
-                           
-                        })
+                ScrollView(.horizontal){
                     HStack{
-                        NavigationLink(destination: {
-                            SecondView
-                        }, label: {
-                            RoundedRectangle(cornerRadius: 20).frame(height: 180)
-                                .overlay(Text("Sa").foregroundColor(.black).background(RoundedRectangle(cornerRadius: 10).fill(Color.red).frame(width:200,height: 120)))
-                           
-                        })
-                    }
-                    
+                        HStack{
+                            NavigationLink(destination: {
+                                FirstView
+                            }, label: {
+                                RoundedRectangle(cornerRadius: 20).frame(width:400,height: 180).foregroundColor(.black)
+                                    .overlay(
+                                        VStack{
+                                            VStack{
+                                                Text("asdasas")
+                                                    .font(.system(size:30).bold())
+                                                    .foregroundColor(.white).background(RoundedRectangle(cornerRadius: 10).fill(Color.red).frame(width:150,height: 40))
+                                            }.offset(y:50)})
+                            })
+                        }
+                        HStack{
+                            RoundedRectangle(cornerRadius: 20).frame(width:400,height: 180).foregroundColor(.black)
+                                .overlay{
+                                    NavigationLink(destination: {
+                                        SecondView
+                                    }, label: {
+                                        Text("as").font(.system(size: 30).bold())
+                                            .foregroundColor(.white).background(RoundedRectangle(cornerRadius: 10).fill(Color.red).frame(width:120,height: 80))
+                                        
+                                    })
+                                }
+                        }
+                    }.padding(.horizontal,2)
+                    Spacer(minLength: 60)
                 }
-                Spacer(minLength: 120)
             }
         }
     }
